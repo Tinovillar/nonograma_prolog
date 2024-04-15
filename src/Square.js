3411a0d8cfa29dff83cb22c53b01d5d8657dbc4a
Tino <valennvillar@gmail.com>
2024-04-15 17:20:01 -0300

    if square is # paint the bg black

--- src/Square.js
+++ src/Square.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Square({ value, onClick }) {
     return (
-        <button className="border bg-white w-10 h-10 rounded" onClick={onClick}>
-            {value !== '_' ? value : null}
+        <button className={`border ${value === '#' ? "bg-black" : "bg-white"} w-10 h-10 rounded`} onClick={onClick}>
+            {value === 'X' ? value : null}
         </button>
     );
 }
