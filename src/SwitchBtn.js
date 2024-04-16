0f1b2e05bcc75a1fba7e933f734c05076ca706bf
Tino <valennvillar@gmail.com>
2024-04-16 01:42:17 -0300

    toggle switch created

--- src/SwitchBtn.js
+++ src/SwitchBtn.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { useState } from 'react';
 
 function SwitchBtn({painting, onClick}) {
+    const [isSelected, setIsSelected] = useState(false);
     return (
-        <button className={`w-36 h-20 ${painting ? "bg-black" : "bg-white"} rounded border-4 border-black`} onClick={onClick}> X
-        </button>
+        // <button className={`w-36 h-20 ${painting ? "bg-black" : "bg-white"} rounded border-4 border-black`} onClick={onClick}> X
+        // </button>
+        <div onClick={()=>{setIsSelected(!isSelected); onClick()}} className={classNames('flex items-center w-44 h-20 cursor-pointer bg-gray-300 m-10 rounded-full',{'bg-white':isSelected})}>
+            <span className={classNames('h-20 w-20 bg-black rounded-full transition-all duration-200', {"ml-24":isSelected})}></span>
+        </div>
     );
 }
 
