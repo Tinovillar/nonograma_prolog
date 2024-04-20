35f072414711161d8cf45ecc264cb9ffc52c3fe5
Tino <valennvillar@gmail.com>
2024-04-20 20:38:54 -0300

    game logic implemented in prolog

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -5,32 +5,86 @@
 
 :-use_module(library(lists)).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-% replace(?fila_a_reemplazar, +nro_fila, +fila_nueva, +grilla, -grilla_Nueva)
-%
-% XsY is the result of replacing the occurrence of X in position XIndex of Xs by Y.
+search_clues(_, [], false).
+search_clues([],[],true).
+search_clues(Clues, [L|Ls], Valid):-
+    L == "X",
+    search_clues(Clues, Ls, Valid),
+    !.
+search_clues(Clues, [L|Ls], Valid):-
+    L == "#",
+    check_clue(Clues, ["#"|Ls], Valid),
+    !.
+search_clues(Clues, [_|Ls], Valid):-
+    search_clues(Clues, Ls, Valid),
+    !.
 
+check_clue([0],[], true):-!.
+check_clue([C|_Cs], [], false):-
+    C > 0,
+    !.
+check_clue([0], _, true):-!.
+check_clue([0],["#"|_Ls], false):-!.
+check_clue([],["#"|_Ls], false):-!.
+check_clue([], _, true):-!.
+check_clue([0|Cs], [L|Ls], Valid):-
+    L == "X",
+    check_clue(Cs, Ls, Valid),!.
+check_clue(_, [L|_], false):-
+    L == "X",
+    !.
+check_clue([C|Cs], [L|Ls], Valid):-
+    L == "#",
+    C > 0,
+    Caux is C - 1,
+    check_clue([Caux|Cs], Ls, Valid),
+    !.
+check_clue(_, [_|_], false).
+
+search(0, [Element|_], Element).
+search(I, [_|Row], Element):-
+    Is is I - 1,
+    search(Is, Row, Element).
+
+col_to_row(_Index,[],[]).
+col_to_row(Index,[Row|Grid],[C|Column]):-
+    search(Index,Row,C),
+    col_to_row(Index,Grid,Column).
+
+clue(0, [C_head|_C_tail], C_head).
+clue(Cursor, [_C_head|C_tail], Return):-
+    Cursor > 0,
+    CursorAux is Cursor - 1,
+    clue(CursorAux, C_tail, Return).
+
+clone([],[]).
+clone([X|Xs], [X|Ys]):-
+    clone(Xs, Ys).
+
+clone_grid([], []).
+clone_grid([X|Xs], [Y|Ys]):-
+    clone(X, Y),
+    clone_grid(Xs, Ys).
+%
 replace(X, 0, Y, [X|Xs], [Y|Xs]).
-
+%
 replace(X, XIndex, Y, [Xi|Xs], [Xi|XsY]):-
     XIndex > 0,
     XIndexS is XIndex - 1,
     replace(X, XIndexS, Y, Xs, XsY).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % put(+Content, +Pos, +RowsClues, +ColsClues, +Grid, -NewGrid, -RowSat, -ColSat).
 %
-put(Content, [RowN, ColN], _RowsClues, _ColsClues, Grid, NewGrid, 0, 0):-
-	% NewGrid is the result of replacing the row Row in position RowN of Grid by a new row NewRow (not yet instantiated).
-	replace(Row, RowN, NewRow, Grid, NewGrid),
-
-	% NewRow is the result of replacing the cell Cell in position ColN of Row by _,
-	% if Cell matches Content (Cell is instantiated in the call to replace/5).	
-	% Otherwise (;)
-	% NewRow is the result of replacing the cell in position ColN of Row by Content (no matter its content: _Cell).			
-	(replace(Cell, ColN, _, Row, NewRow),
-	Cell == Content
-		;
-	replace(_Cell, ColN, Content, Row, NewRow)).
+put(Content, [RowN, ColN], RowsClues, ColsClues, Grid, NewGrid, RowSat, ColSat):-
+    replace(Row, RowN, NewRow, Grid, NewGrid),
+    (replace(Cell, ColN, _, Row, NewRow),
+    Cell == Content
+        ;
+    replace(_Cell, ColN, Content, Row, NewRow)),
+	clone_grid(NewGrid, ClonedGrid),
+    clone(NewRow, ClonedRow),
+    clue(RowN, RowsClues, RowClue),
+    clue(ColN, ColsClues, ColClue),
+    col_to_row(ColN, ClonedGrid, Column),
+    search_clues(RowClue, ClonedRow, RowSat),
+    search_clues(ColClue, Column, ColSat).
