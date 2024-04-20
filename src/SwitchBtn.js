2d5a8aee67fa625c7415eefd0cf0ede41f4587a0
Tino <valennvillar@gmail.com>
2024-04-20 20:48:07 -0300

    X character to the button added

--- src/SwitchBtn.js
+++ src/SwitchBtn.js
@@ -7,7 +7,9 @@ function SwitchBtn({painting, onClick}) {
         // <button className={`w-36 h-20 ${painting ? "bg-black" : "bg-white"} rounded border-4 border-black`} onClick={onClick}> X
         // </button>
         <div onClick={()=>{setIsSelected(!isSelected); onClick()}} className={classNames('flex items-center w-44 h-20 cursor-pointer bg-gray-300 rounded-full')}>
-            <span className={classNames('h-20 w-20 bg-black rounded-full transition-all duration-200', {"ml-24":isSelected})}></span>
+            <span className={classNames('h-20 w-20 bg-black text-4xl font-bold border text-center flex items-center justify-center rounded-full transition-all duration-200', {"ml-24 bg-white":isSelected})}>
+                {isSelected ? "X" : ""}
+            </span>
         </div>
     );
 }
