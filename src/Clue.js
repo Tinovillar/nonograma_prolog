2823821fea1c1d440044974a2fcf414007fc270e
Tino <valennvillar@gmail.com>
2024-04-20 20:41:26 -0300

    changing background depending on it was satisfied

--- src/Clue.js
+++ src/Clue.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Clue({ clue, isColumn }) {
+function Clue({ clue, isColumn, done }) {
     return (
-        <div className={`flex ${isColumn ? "flex-col" : ''} items-center justify-evenly text-center w-20 h-20 bg-blue-200 rounded-md m-1 border`} >
+        <div className={`flex ${isColumn ? "flex-col" : ''} items-center justify-evenly text-center w-20 h-20 ${done ? 'bg-green-400' : 'bg-blue-200'} rounded-md m-1 border`} >
             {clue.map((num, i) =>
                 <div key={i} className='text-lg w-auto h-auto'>
                     {num}
