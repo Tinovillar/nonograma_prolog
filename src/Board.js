bf94c061f5ec452ada0354492df59a1b743906b8
Tino <valennvillar@gmail.com>
2024-04-28 14:40:55 -0300

    grid style used for responsive desing

--- src/Board.js
+++ src/Board.js
@@ -6,34 +6,31 @@ function Board({ grid, rowsClues, colsClues, onClick, rowSat, colSat}) {
     const numOfRows = grid.length;
     const numOfCols = grid[0].length;
     return (
-        <div className="flex-column">
-            <div className={`flex justify-around`}>
-                <div className='w-12 h-12 m-11'>{/* top-left corner square */}</div>
+        <div className="grid grid-rows-4 grid-cols-4 items-center align-center justify-items-center w-full">
+            <div className={`flex justify-around col-start-2 col-span-4 w-full h-full`}>
                 {colsClues.map((clue, i) =>
                     <Clue clue={clue} key={i} isColumn={true} done={colSat.includes(i)}/>
                 )}
             </div>
-            <div className="flex">
-                <div className={`flex flex-col justify-between`}>
-                    {rowsClues.map((clue, i) =>
-                        <Clue clue={clue} key={i} isColumn={false} done={rowSat.includes(i)}/>
-                    )}
-                </div>
-                <div className="grid" style={{
-                        gridTemplateColumns: `repeat(${numOfCols}, minmax(0, 1fr))`,
-                        gridTemplateRows: `repeat(${numOfRows}, minmax(0,1fr))`
-                    }}
-                >
-                    {grid.map((row, i) =>
-                        row.map((cell, j) =>
-                            <Square
-                                value={cell}
-                                onClick={() => onClick(i, j)}
-                                key={i + j}
-                            />
-                        )
-                    )}
-                </div>
+            <div className={`flex flex-col justify-between row-start-2 row-span-4 w-full h-full`}>
+                {rowsClues.map((clue, i) =>
+                    <Clue clue={clue} key={i} isColumn={false} done={rowSat.includes(i)}/>
+                )}
+            </div>
+            <div className="grid row-start-2 row-span-4 col-start-2 col-span-4 w-full h-full" style={{
+                    gridTemplateColumns: `repeat(${numOfCols}, minmax(0, 1fr))`,
+                    gridTemplateRows: `repeat(${numOfRows}, minmax(0,1fr))`
+                }}
+            >
+                {grid.map((row, i) =>
+                    row.map((cell, j) =>
+                        <Square
+                            value={cell}
+                            onClick={() => onClick(i, j)}
+                            key={i + j}
+                        />
+                    )
+                )}
             </div>
         </div>
     );
