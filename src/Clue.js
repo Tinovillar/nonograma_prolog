7e1abec152801bd2807da8b7a54f262a867a90c5
Tino <valennvillar@gmail.com>
2024-04-28 14:42:47 -0300

    width and height removed

--- src/Clue.js
+++ src/Clue.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Clue({ clue, isColumn, done }) {
     return (
-        <div className={`flex ${isColumn ? "flex-col h-32 w-12" : 'w-32 h-12'} items-center justify-evenly text-center ${done ? 'bg-green-400' : 'bg-blue-200'} rounded-md m-1 border`} >
+        <div className={`flex ${isColumn ? "flex-col" : 'h-full'} items-center justify-evenly text-center ${done ? 'bg-green-400' : 'bg-blue-200'} rounded-md w-full border`} >
             {clue.map((num, i) =>
                 <div key={i} className='text-lg'>
                     {num}
--- src/Square.js
+++ src/Square.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Square({ value, onClick }) {
     return (
-        <button className={`border-2 ${value === '#' ? "bg-black" : "bg-white"} h-12 w-12 rounded-md m-1`} onClick={onClick}>
+        <button className={`border-2 ${value === '#' ? "bg-black" : "bg-white"} rounded-md`} onClick={onClick}>
             {value === 'X' ? value : null}
         </button>
     );
